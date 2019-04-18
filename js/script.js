000d3527855e1c5ee439a7e0cb31decf30ce9500
Lanai <lanai.lewis@gmail.com>
2019-04-17 17:53:39 -0700

    Initial Commit at Start of Project

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// When the page first loads, the first text field should be in focus by default
+
+$("#name").focus();
+
+// Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu
+
