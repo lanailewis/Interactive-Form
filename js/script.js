cfa60c07a108d0e0390186d620fd61c8ec98ea1a
Lanai <lanai.lewis@gmail.com>
2019-04-18 12:17:54 -0700

    Update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,42 @@ $("#name").focus();
 
 // Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu
 
+// Give the field an id of “other-title,” and add the placeholder text of "Your Job Role"
+
+$('#other-title').hide();
+
+// For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu
+
+//if js puns is selected
+//hide tomato, steel blue, dim grey
+
+//$('#design').val('js-puns');
+//$('#color').val('tomato').hide();
+
+$("#design").change((e)=>{
+	
+		if ( $(e.target).val() == "js puns" ) { 
+			$("#color tomato").hide();
+        }
+});
+
+//if heart js is selected
+//hide cornflower blue, dark slate grey, gold
+
+// If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+
+// If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+
+// When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.
+
+
+	
+
+
+
+	
+
+
+
+
+
