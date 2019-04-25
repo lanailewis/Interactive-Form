ac55c52368a51fe68683a74be27227176daf3672
Lanai <lanai.lewis@gmail.com>
2019-04-25 13:12:00 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,32 +16,53 @@ $('#other-title').hide();
 //if heart js is selected
 //hide cornflower blue, dark slate grey, gold
 
-const cornflowerBlue = $('#color option[value="cornflowerblue"]').addClass('cornflower-color');
-const tomato = $('#color option[value="tomato"]').addClass('tomato-color');
 
 $("#design").change((e)=>{
 	
 		if ( $(e.target).val() == "js puns" ) { 
-			document.getElementsByClassName('cornflower-blue').selected = true;
 			$('#color option[value="tomato"],[value="steelblue"],[value="dimgrey"]').hide();
 			$('#color option[value="cornflower blue"],[value="darkslategrey"],[value="gold"]').show();
+			$('#color option[value="cornflower blue"]').attr('selected', true);
+			$('#color option[value="tomato"]').attr('selected', false);
         }
 		if ( $(e.target).val() == "heart js" ) { 
-			document.getElementsByClassName('tomato').selected = true;
 			$('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').hide();
 			$('#color option[value="tomato"],[value="steelblue"],[value="dimgrey"]').show();
+			$('#color option[value="tomato"]').attr('selected', true);
+			$('#color option[value="cornflower blue"]').attr('selected', false);
         }
 		
 		
 });
 
+// Some events are at the same day and time as others
+
+// If the user selects a workshop, don't allow selection of a workshop at the same day and time
+
+	// You should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available
+	
+// When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled
+
+// As a user selects activities, a running total should display below the list of checkboxes
+
+	// For example, if the user selects "Main Conference", then Total: $200 should appear
+	// If they add 1 workshop, the total should change to Total: $300
+	
+/* $(".activities").click((e)=>{
+	if $('input.js-frameworks').attr('checked'){
+		$('input.express').attr('disabled', true);
+	}
+}); */
+
+$('input.js-frameworks').click((e)=>{
+    $("input.express").toggle(e.target.checked);
+});
+	
 
 
-// If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
 
-// If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
 
-// When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.
+
 
 
 	
