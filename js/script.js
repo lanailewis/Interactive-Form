d25f1cfffc1a9262456a3b152fdd2277e3d2b93c
Lanai <lanai.lewis@gmail.com>
2019-05-08 12:40:33 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,20 +35,6 @@ $("#design").change((e)=>{
 		
 });
 
-// Some events are at the same day and time as others
-
-// If the user selects a workshop, don't allow selection of a workshop at the same day and time
-
-	// You should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available
-	
-// When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled
-
-// As a user selects activities, a running total should display below the list of checkboxes
-
-	// For example, if the user selects "Main Conference", then Total: $200 should appear
-	
-	// If they add 1 workshop, the total should change to Total: $300
-
 const $jsFrameworks = $(".activities input[name='js-frameworks']");
 const $jsLibs = $(".activities input[name='js-libs']");
 const $express = $(".activities input[name='express']");
@@ -56,10 +42,6 @@ const $node = $(".activities input[name='node']");
 const $buildTools = $(".activities input[name='build-tools']");
 const $npm = $(".activities input[name='npm']");
 
-
-// create and append element to store the total activity cost - helpful to give it an id for easy targeting and updating of its innerHTML later.
-
-// create variable to store total cost - initially set to 0 - don't use const since you want to update this periodically.
 
 const $activities = $('.activities');
 const div = document.createElement('div');
@@ -75,25 +57,6 @@ $($activities).change((e)=>{
 	
 });
 
-
-
-// change event listener for activity section - event delegation is helpful here
-
-// variable to store dollar amount of checkbox that was just checked or unchecked - use DOM traversal to target the label parent of the checkbox, and something like regex or indexOf to grab the numbers between the $ and the end of the string.
-
-// variable to store the day and time of checkbox that was just checked or unchecked - similar to above, use DOM traversal and regex or indexOf to target the substring between the em dash and the comma.
-  // conditional to see if box was checked or unchecked
-  // if box was checked 
-    // add cost to total
-    // loop over all checkboxes
-     // conditional to see if checkboxes[i] matches day and time of checkbox that was checked and if so, disable checkboxes[i]
-  
-  // if box was unchecked
-    // subtract cost from total
-    // loop over all checkboxes
-     // conditional to see if checkboxes[i] matches day and time of checkbox that was checked and if so, enable checkboxes[i]
-
-  // lastly, inside the event listener, update the innerHTML of the cost element you appended to equal the adjusted cost total  
 
 
 
