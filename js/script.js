f9d6911f5a930cb32d0f2ae098bc594e5881bed3
Lanai <lanai.lewis@gmail.com>
2019-09-16 13:57:24 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ $("#name").focus();
 // Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu
 
 $('#other-title').hide();
+	$("#title").change((e)=>{
+		if( $(e.target).val() == "other" ) {
+			$('#other-title').show();
+		}
+});
+
 
 /***************
 T-Shirt Section
